61ba4298e74191341adb63e8b771d658c16ac058
K4ne <nick@getmighty.net>
2022-04-28 19:55:59 -0500

    Got it working after extensive rechecking

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 
 // Import our modular routers for /tips and /feedback
 const notesRouter = require('./notes');
-const diagnosticsRouter = require('./diagnostics');
 // TODO: import your diagnostics route
+
 const app = express();
-app.use('/diagnostics', diagnosticsRouter);
 
 app.use('/notes', notesRouter);
 
-module.exports = express;
+module.exports = app;
